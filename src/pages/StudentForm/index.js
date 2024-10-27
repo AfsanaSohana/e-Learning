5f91a719e8d14320f6ec4ff7aa044a9dd15b0bd0
AfsanaSohana <sohanaafsana16@gmail.com>
2024-10-27 14:30:45 +0600

    27 0ct 24

--- src/pages/StudentForm/index.js
+++ src/pages/StudentForm/index.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import {useParams} from "react-router-dom";
 
 const StudentForm = () => {
-    const [formData, setFormData] = useState({id:'',student_name:'',email:'',address:'',contact_number:'',photo:'',password:''});
+    const [inputs, setInputs] = useState({id:'',student_name:'',email:'',address:'',contact_number:'',photo:'',password:''});
     const [selectedfile, setSelectedFile] = useState([]);
   const navigate=useNavigate();
 
@@ -13,7 +13,7 @@ const StudentForm = () => {
   
   function getDatas(){
       axios.get(`${process.env.REACT_APP_API_URL}/student/${id}`).then(function(response) {
-          setFormData(response.data.data);
+          setInputs(response.data.data);
       });
   }
 
@@ -26,12 +26,12 @@ const StudentForm = () => {
   const handleChange = (event) => {
       const name = event.target.name;
       const value = event.target.value;
-      setFormData(values => ({...values, [name]: value}));
+      setInputs(values => ({...values, [name]: value}));
   }
 
   // const handleSubmit = async(e) => {
   //     e.preventDefault();
-  //     console.log(formData)
+  //     console.log(inputs)
   
 
       const handelFile = (e) => {
@@ -40,20 +40,20 @@ const StudentForm = () => {
       const handleSubmit = async (e) => {
           e.preventDefault();
   
-          const formData = new FormData();
+          const inputs = new FormData();
   
           for (let i = 0; i < selectedfile.length; i++) {
-              formData.append('files[]', selectedfile[i])
+              inputs.append('files[]', selectedfile[i])
           }
   
-          for (const property in formData) {
-              formData.append(property, formData[property])
+          for (const property in inputs) {
+              inputs.append(property, inputs[property])
           }
       
       try{
           let apiurl='';
-          if(formData.id!=''){
-              apiurl=`/student/edit/${formData.id}`;
+          if(inputs.id!=''){
+              apiurl=`/student/edit/${inputs.id}`;
           }else{
               apiurl=`/Student/create`;
           }
@@ -61,8 +61,8 @@ const StudentForm = () => {
           let response= await axios({
               method: 'post',
               responsiveTYpe: 'json',
-              url: `${process.env.REACT_APP_API_URL}${apiurl,formData}`,
-              data: formData
+              url: `${process.env.REACT_APP_API_URL}${apiurl,inputs}`,
+              data: inputs
           });
           console.log(response)
           navigate('/student')
@@ -81,31 +81,31 @@ const StudentForm = () => {
 
             <div className="mb-3">
               <label htmlFor="student_name" className="form-label text-primary">Name</label>
-              <input type="text" className="form-control" id="student_name" name="student_name" value={formData.student_name} onChange={handleChange}
+              <input type="text" className="form-control" id="student_name" name="student_name"  defaultValue={inputs.student_name} onChange={handleChange}
                 placeholder="Enter student's name"/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="email" className="form-label text-primary">Email</label>
               <input type="email" className="form-control" id="email" name="email"
-                value={formData.email} onChange={handleChange} placeholder="Enter student's email"/>
+                defaultValue={inputs.email} onChange={handleChange} placeholder="Enter student's email"/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="address" className="form-label text-primary">Address</label>
               <input type="text" className="form-control" id="address"
-                name="address" value={formData.address}onChange={handleChange}  placeholder="Enter student's address"/>
+                name="address"defaultValue={inputs.address}onChange={handleChange}  placeholder="Enter student's address"/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="photo" className="form-label text-primary"> Photo</label>
-              <input type="file" className="form-control" id="photo" name="photo" onChange={handleChange}/>
+              <input type="file" className="form-control" id="photo" name="photo"  multiple defaultValue={inputs.photo} onChange={handleChange}/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="password" className="form-label text-primary">Password </label>
               <input type="password" className="form-control" id="password" name="password"
-                value={formData.password} onChange={handleChange} placeholder="Enter a password"/>
+               defaultValue={inputs.password} onChange={handleChange} placeholder="Enter a password"/>
             </div>
             <button type="submit" className="btn btn-primary w-100"> Submit
 </button>
@@ -118,3 +118,4 @@ const StudentForm = () => {
 };
 
 export default StudentForm;
+ 
